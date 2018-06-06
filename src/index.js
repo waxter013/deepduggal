962924143cb9deb217a6aea1c4b93bd5dc763427
waxter013 <duggald14@gmail.com>
2018-06-06 11:13:07 -0400

    Used react-snapshot to SSR the website

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { render } from 'react-snapshot';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    render( <App />, document.getElementById('root') )
+);
 registerServiceWorker();
